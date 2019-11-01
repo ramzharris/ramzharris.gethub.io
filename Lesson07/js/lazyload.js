b6b63fdc67294794a2742215527c7a502534b989
ramzharris <har18152@byui.edu>
2019-10-31 23:08:01 -0600

    lazyloading

---
+++ Lesson07/js/lazyload.js
@@ -0,0 +1,34 @@
+const images = document.querySelectorAll("[data-src]");
+
+function noAttribute(img) {
+        const remove = img.removeAttribute("data-src");
+};
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }    
+    img.src = src;
+};
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+            entry.onload = noAttribute(entry.target);
+        };
+    });
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
