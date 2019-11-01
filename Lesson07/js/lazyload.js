3ddeb123b1fda127d7565063dc3b6b1e17366141
ramzharris <har18152@byui.edu>
2019-10-31 23:31:46 -0600

    submission

--- Lesson07/js/lazyload.js
+++ Lesson07/js/lazyload.js
@@ -1,34 +1,25 @@
-const images = document.querySelectorAll("[data-src]");
-
-function noAttribute(img) {
-        const remove = img.removeAttribute("data-src");
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }    
-    img.src = src;
-};
-
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
-};
-
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-            entry.onload = noAttribute(entry.target);
-        };
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
-}, imgOptions);
-
-images.forEach(image => {
-    imgObserver.observe(image);
-});
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
